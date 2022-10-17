0d1e36300b2f1bc902e3383dd4a56d9dc132202b
zetscape <113311167+zetscape@users.noreply.github.com>
2022-10-18 01:20:31 +0300

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,7 @@
+// JavaScript Document
+function showmenu(){
+	document.getElementById( 'menuoverlay' ).style.display="block";
+	}
+function hidemenu(){
+	document.getElementById( 'menuoverlay' ).style.display="none";
+	}
